01797b60c6e4b60dcf9ff0f3629dc35c9761ab85
Tawnee <allprt89@gmail.com>
2021-11-10 20:39:04 -0700

    connect to firebase

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import EventList from '../components/events/eventList'
-import { getFeaturedEvents } from '../dummy-data'
-export default function HomePage() {
-	const featuredEvents = getFeaturedEvents()
+import { getFeaturedEvents } from '../helpers/api-utils'
 
+export default function HomePage({ events }) {
 	return (
 		<div>
-			<EventList items={featuredEvents} />
+			<EventList items={events} />
 		</div>
 	)
+}
+
+export async function getStaticProps() {
+	const featuredEvents = await getFeaturedEvents()
+
+	return {
+		props: {
+			events: featuredEvents,
+		},
+	}
 }

0be128bf69bf62044008ea402cb505a3fe695055
Tawnee <allprt89@gmail.com>
2021-11-11 18:46:18 -0700

    connect eventId page to firebase

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,11 @@
-import { useRouter } from 'next/router'
 import Button from '../../components/ui/button'
 import ErrorAlert from '../../components/events/error-alert'
 import EventSummary from '../../components/event-detail/event-summary'
 import EventLogistics from '../../components/event-detail/event-logistics'
 import EventContent from '../../components/event-detail/event-content'
-import { getEventById } from '../../dummy-data'
+import { getEventById, getAllEvents } from '../../helpers/api-utils'
 
-export default function EventDetailPage() {
-	const { query } = useRouter()
-	const eventId = query.eventId
-	const event = getEventById(eventId)
-
+export default function EventDetailPage({ event }) {
 	if (!event) {
 		return (
 			<div className='center'>
@@ -36,4 +31,25 @@ export default function EventDetailPage() {
 			</EventContent>
 		</>
 	)
+}
+
+export async function getStaticProps(context) {
+	const eventId = context.params.eventId
+	const event = await getEventById(eventId)
+
+	return {
+		props: {
+			event,
+		},
+	}
+}
+
+export async function getStaticPaths() {
+	const events = await getAllEvents()
+	const paths = events.map((event) => ({ params: { eventId: event.id } }))
+
+	return {
+		paths,
+		fallback: false,
+	}
 }
